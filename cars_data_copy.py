63391265d37538fecc92b15d0e510dc15548d160
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-11-01 10:33:03 +0530

    Update cars_data_copy.py

--- cars_data_copy.py
+++ cars_data_copy.py
@@ -7,6 +7,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
 
 
 
@@ -22,6 +24,39 @@ uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["cs
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 st.write(df)
+
+
+
+if uploaded_file is not None:
+    @st.cache
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Pandas Profiling Report**')
+    st_profile_report(pr)
+else:
+    st.info('Awaiting for CSV file to be uploaded.')
+    if st.button('Press to use Example Dataset'):
+        # Example data
+        @st.cache
+        def load_data():
+            a = pd.DataFrame(
+                np.random.rand(100, 5),
+                columns=['a', 'b', 'c', 'd', 'e']
+            )
+            return a
+        df = load_data()
+        pr = ProfileReport(df, explorative=True)
+        st.header('**Input DataFrame**')
+        st.write(df)
+        st.write('---')
+        st.header('**Pandas Profiling Report**')
+        st_profile_report(pr)
 
 
 # In[16]:
