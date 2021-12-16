aa24be49137bed95a202c0ebc8f7cd5ad84221a5
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-12-16 06:58:43 +0530

    Add files via upload

---
+++ MBA_analysis.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+
+# In[12]:
+
+
+st.write("""
+# MARKET BASKET ANALYSIS """)
+
+
+# In[ ]:
+
+
+df=pd.read_excel("MBA_data_1.xlsx")
+
+
+# In[11]:
+
+
+st.write(df)
+
+
+# In[ ]:
+
+
+
+
