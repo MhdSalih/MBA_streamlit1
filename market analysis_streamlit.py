c9e098fbbae4b55cdb5b9a272b7b39ce9164ae1c
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-12-18 11:21:55 +0530

    Update market analysis_streamlit.py

--- market analysis_streamlit.py
+++ market analysis_streamlit.py
@@ -4,34 +4,34 @@
 # In[10]:
 
 
+import streamlit as st
 import pandas as pd
 import numpy as np
-import streamlit as st
 
 
-# In[11]:
-
-
-st.write("""
-# MARKET BASKET ANALYSIS """)
-
-
-# In[14]:
-
-
-df=pd.read_csv("new_MBA.csv")
-df = df.drop(df.columns[0], axis=1)
-df
 
 
 # In[15]:
 
 
+st.title('MARKET BASKET ANALYSIS')
+st.write('')
+
+upload_file=st.sidebar.file_uploader(label="upload your csv or excel file",type=["csv","xlsx"])
+
+global df
+if upload_file is not None:
+  print(upload_file)
+  print('hello')
+  try:
+    df = pd.read_csv(upload_file)
+  except Exception as e:
+    print(e)
+    df = pd.read_excel(uploade_file)
+    
+
+
+# In[16]:
+
+
 st.write(df)
-
-
-# In[ ]:
-
-
-
-
