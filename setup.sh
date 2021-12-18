a84d2ee9f0a2540f27ca8d0d05e3da2cd3fc4f45
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-12-18 14:11:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
-FROM python:3.8.12
-COPY . /app
-WORKDIR /app
-RUN pip install -r requirements.txt
-EXPOSE 8501
-RUN mkdir ~/.streamlit
-RUN cp config.toml ~/.streamlit/config.toml
-RUN cp credentials.toml ~/.streamlit/credentials.toml
-RUN apt-get update ##[edited]
-RUN apt-get install ffmpeg libsm6 libxext6  -y
-WORKDIR /app
-ENTRYPOINT ["streamlit", "run", "app.py"]
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
